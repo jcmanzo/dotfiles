e3d6b67be5c55c4060992c99901460cf7c2c835a
JC <jcmanzo@github.com>
2022-12-21 12:47:08 -0500

    Update git main branch check

--- .oh-my-zsh/custom/plugins/jc-git/jc-git.plugin.zsh
+++ .oh-my-zsh/custom/plugins/jc-git/jc-git.plugin.zsh
@@ -2,15 +2,9 @@
 
 # Check if main exists and use instead of master
 function git_main_branch() {
-  command git rev-parse --git-dir &>/dev/null || return
-  local ref
-  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk}; do
-    if command git show-ref -q --verify $ref; then
-      echo ${ref:t}
-      return
-    fi
-  done
-  echo master
+  if [ -d .git ]; then
+    git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'
+  fi
 }
 
 # Borrowed from https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/gitfast/gitfast.plugin.zsh
